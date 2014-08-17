133641909d8b920a23a39c055441e37f19a57bde
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-08-17 00:10:55 -0700

    pass stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,48 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	 if(obj === null || obj === undefined) {
+	 	return 'null';
+	 }
+	 else if(typeof obj === 'number'){
+	 	return obj.toString(); 
+	 }
+	 else if(typeof obj === 'boolean'){
+	 	return obj ? 'true' : 'false';
+	 }
+	 else if(typeof obj === 'function'){
+	 	return false;
+	 }
+	 else if(typeof obj === 'string'){
+	 	return '"' + obj + '"';
+	 }
+	 else if(_.isArray(obj)){
+	 	if(obj.length === 0){
+	 		return '[]';
+	 	}
+	 	else{
+	 		var result = [];
+	 		for(var i = 0; i < obj.length; i++){
+	 			if(typeof obj[i] !== 'function' && obj[i] !== undefined){
+	 				result.push(stringifyJSON(obj[i]));
+	 			}
+	 		}
+	 		return '[' + result.join(',') + ']';
+	 	}
+	 }
+	 else{
+	 	if(_.isEmpty(obj)){
+	 		return '{}';
+	 	}
+	 	else{
+	 		var result = []; 
+	 		for(var k in obj){
+	 			if(typeof obj[k] !== 'function' && obj[k] !== undefined){
+	 				result.push(stringifyJSON(k) + ":" + stringifyJSON(obj[k]));
+	 			}
+	 		}
+	 		return '{' + result.join(',') + '}';
+	 	}
+	 }
+
 };
