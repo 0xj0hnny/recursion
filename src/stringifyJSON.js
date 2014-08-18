10857edc174a9258669e30e47d7de8652b62981f
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-08-17 18:09:28 -0700

    updated stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,48 +3,61 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-	 if(obj === null || obj === undefined) {
-	 	return 'null';
-	 }
-	 else if(typeof obj === 'number'){
-	 	return obj.toString(); 
-	 }
-	 else if(typeof obj === 'boolean'){
-	 	return obj ? 'true' : 'false';
-	 }
-	 else if(typeof obj === 'function'){
-	 	return false;
-	 }
-	 else if(typeof obj === 'string'){
-	 	return '"' + obj + '"';
-	 }
-	 else if(_.isArray(obj)){
-	 	if(obj.length === 0){
-	 		return '[]';
-	 	}
-	 	else{
-	 		var result = [];
-	 		for(var i = 0; i < obj.length; i++){
-	 			if(typeof obj[i] !== 'function' && obj[i] !== undefined){
-	 				result.push(stringifyJSON(obj[i]));
-	 			}
-	 		}
-	 		return '[' + result.join(',') + ']';
-	 	}
-	 }
-	 else{
-	 	if(_.isEmpty(obj)){
-	 		return '{}';
-	 	}
-	 	else{
-	 		var result = []; 
-	 		for(var k in obj){
-	 			if(typeof obj[k] !== 'function' && obj[k] !== undefined){
-	 				result.push(stringifyJSON(k) + ":" + stringifyJSON(obj[k]));
-	 			}
-	 		}
-	 		return '{' + result.join(',') + '}';
-	 	}
-	 }
+	if(obj === null || obj === undefined) {
+		return 'null';
+	}
+	switch(typeof obj){
+		
+		case 'number':
+			return obj.toString(); 
+			break; 
+		
+		case 'boolean':
+			return obj ? 'true' : 'false';
+			break;
+
+		case 'function':
+			return false;
+			break;
+
+		case 'string':
+			return '"' + obj + '"';
+			break;
+			
+		case 'object':
+			if(_.isArray(obj)){
+				if(obj.length === 0){
+					return '[]';
+				}
+				else{
+					var result = [];
+					for(var i = 0; i < obj.length; i++){
+						if(typeof obj[i] !== 'function' && obj[i] !== undefined){
+							result.push(stringifyJSON(obj[i]));
+						}
+					}
+					return '[' + result.join(',') + ']';
+				}
+			}
+			else{
+				if(_.isEmpty(obj)){
+					return '{}';
+				}
+				else{
+					var result = []; 
+					for(var k in obj){
+						if(typeof obj[k] !== 'function' && obj[k] !== undefined){
+							result.push(stringifyJSON(k) + ":" + stringifyJSON(obj[k]));
+						}
+					}
+					return '{' + result.join(',') + '}';
+				}
+			}
+		break;
+
+	default: 
+		return obj;
+		break;
+	}
 
 };
