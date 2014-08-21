c5403430c4f318c5f5d3a9f6dab5278da389350e
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-08-21 01:07:25 -0700

    finished getElementByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+//function walkTheDOM(node, func) {
+//    func(node);
+//    node = node.firstChild;
+//    while (node) {
+//        walkTheDOM(node, func);
+//        node = node.nextSibling;
+//    }
+//}
+
 var getElementsByClassName = function(className){
-  // your code here
+  var result = []; 
+  //check if document.body contains elemet with className
+  if(document.body.classList.contains(className)){
+  	result.push(document.body);
+  }
+
+  //check if other level of the DOM contains the element with the className
+  var checkChild = function(node){
+  	var child = node.childNodes;
+  	if(child.length > 0){
+  		for(var i = 0; i < child.length; i++){
+  			if(child[i] instanceof HTMLElement && child[i].classList.contains(className)){
+  				result.push(child[i]);
+  			}
+  			checkChild(child[i]);
+  		}
+  	}
+  };
+
+  checkChild(document.body);
+  return result; 
 };
